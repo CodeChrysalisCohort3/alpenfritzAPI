1793493709fc91aebb3c2e4d8b30476e6cdf90d0
alpenfritz <mymailismissing@gmail.com>
2018-01-28 21:02:49 +0900

    List all records with GET /api/v1/tvshows

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const knex = require('../db/knex');
 const app = require('../app');
 const expect = require('chai').expect;
+const fixtures = require('./fixtures');
 
 describe('CRUD API', () => {
   before((done) => {
@@ -14,6 +15,21 @@ describe('CRUD API', () => {
         return knex.seed.run();
       })
       .then(() => done());
+  });
+  
+  // GET all entries
+  it('should list all records (GET method)', (done) => {
+    request(app)
+      .get('/api/v1/tvshows') // GET
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        // console.log(response.body); // copy this to ./test/fixtures.js
+        expect(response.body).to.be.a('array');
+        expect(response.body).to.deep.equal(fixtures.tvshows);
+        done();
+      });
   });
 
 });
---
+++ test/fixtures.js
@@ -0,0 +1,69 @@
+const tvshows = [
+  {
+    id: 1,
+    title: 'Band of Brothers',
+    description: 'Action, Drama, History',
+    rating: 9.5,
+    url: 'http://www.imdb.com/title/tt0185906/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=3499204302&pf_rd_r=1D85Y5C0AWD7AFSNGQCM&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=toptv&ref_=chttvtp_tt_2'
+  },
+  {
+    id: 2,
+    title: 'Game of Thrones',
+    description: 'Action, Adventure, Drama',
+    rating: 9.4,
+    url: 'http://www.imdb.com/title/tt0944947/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=3499204302&pf_rd_r=1D85Y5C0AWD7AFSNGQCM&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=toptv&ref_=chttvtp_tt_4'
+  },
+  {
+    id: 3,
+    title: 'Breaking Bad',
+    description: 'Crime, Drama, Thriller',
+    rating: 9.5,
+    url: 'http://www.imdb.com/title/tt0903747/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=3499204302&pf_rd_r=1D85Y5C0AWD7AFSNGQCM&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=toptv&ref_=chttvtp_tt_5'
+  },
+  {
+    id: 4,
+    title: 'The Wire',
+    description: 'Crime, Drama, Thriller',
+    rating: 9.3,
+    url: 'http://www.imdb.com/title/tt0306414/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=3499204302&pf_rd_r=1D85Y5C0AWD7AFSNGQCM&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=toptv&ref_=chttvtp_tt_6'
+  },
+  {
+    id: 5,
+    title: 'True Detective',
+    description: 'Crime, Drama, Mystery',
+    rating: 9,
+    url: 'http://www.imdb.com/title/tt2356777/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=3499204302&pf_rd_r=1D85Y5C0AWD7AFSNGQCM&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=toptv&ref_=chttvtp_tt_20'
+  },
+  {
+    id: 6,
+    title: 'Fargo',
+    description: 'Crime, Drama, Thriller',
+    rating: 9,
+    url: 'http://www.imdb.com/title/tt2802850/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=3499204302&pf_rd_r=1D85Y5C0AWD7AFSNGQCM&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=toptv&ref_=chttvtp_tt_23'
+  },
+  {
+    id: 7,
+    title: 'Stranger Things ',
+    description: 'Drama, Fantasy, Horror',
+    rating: 9,
+    url: 'http://www.imdb.com/title/tt4574334/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=3499204302&pf_rd_r=1D85Y5C0AWD7AFSNGQCM&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=toptv&ref_=chttvtp_tt_33'
+  },
+  {
+    id: 8,
+    title: 'Black Mirror',
+    description: 'Drama, Sci-Fi, Thriller',
+    rating: 8.9,
+    url: 'http://www.imdb.com/title/tt2085059/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=3499204302&pf_rd_r=1D85Y5C0AWD7AFSNGQCM&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=toptv&ref_=chttvtp_tt_35'
+  },
+  {
+    id: 9,
+    title: 'Westworld',
+    description: 'Drama, Mystery, Sci-Fi',
+    rating: 8.9,
+    url: 'http://www.imdb.com/title/tt0475784/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=3499204302&pf_rd_r=1D85Y5C0AWD7AFSNGQCM&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=toptv&ref_=chttvtp_tt_36'
+  }
+];
+
+module.exports = {
+  tvshows
+}
